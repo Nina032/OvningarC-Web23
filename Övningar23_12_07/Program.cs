278bcddbb0c99c385f7acb359d8e170d82c4a8ea
Nevena <nevena.kicanovic@gmail.com>
2023-12-13 12:18:35 +0100

    published

---
+++ Övningar23_12_07/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Övningar23_12_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Laws of freach */
+            //int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+            //int currentMin = int.MaxValue;
+
+            //foreach (int value in array)
+            //{
+            //    if (value < currentMin)
+            //        currentMin = value;
+            //}
+            //Console.WriteLine(currentMin);
+
+            //int total = 0;
+
+            //foreach (int value in array)
+            //    total += value;
+
+            //float average = (float)total / array.Length;
+
+            //Console.WriteLine(average);
+
+            string[] usernames = new string[100]; //Antalet användare begränsat till 100
+            string[] passwords = new string[100];
+            int userCount = 0; //Håller reda på antalet användare
+
+            while (true)
+            {
+                Console.WriteLine("Väj en åtgärd: ");
+                Console.WriteLine("1. Sign in");
+                Console.WriteLine("2. Log in");
+                Console.WriteLine("3. Avsluta");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("== Sign In ==");
+                        Console.WriteLine("Ange ett användarnamn: ");
+                        string newUsername = Console.ReadLine();
+                        Console.WriteLine("Ange ett lösenord: ");
+                        string newPassword = Console.ReadLine();
+
+                        //Lägg till användaren i arrayerna
+                        usernames[userCount] = newUsername;
+                        passwords[userCount] = newPassword;
+
+                        userCount++; //Öka antalet användare
+                        Console.WriteLine("Du har skapat ett konto. " +
+                            "Gå tillbaka till huvudmenyn och logga in");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("== Log In ==");
+                        Console.WriteLine("Användarnamn: ");
+                        string username = Console.ReadLine();
+
+                        Console.WriteLine("Lösenord: ");
+                        string password = Console.ReadLine(); 
+
+                        bool loginSuccessful = false;
+
+                        for (int i = 0; i < userCount; i++) 
+                        {
+                            if (usernames[i] == username && passwords[i] == password)
+                            {
+                                loginSuccessful = true;
+                                break;
+                            }
+                        }
+                        if (loginSuccessful)
+                        {
+                            Console.WriteLine("Inloggning lyckades!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fell användarnamn eer lösenord. Försök igen.");
+                        }
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Avslutar programmet. Hejdå!");
+                        return;
+
+                    default:
+                        Console.WriteLine("Ogiltigt val. Försök igen.");
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Övningar23_12_12/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace Övningar23_12_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.Metoden Sort*/
+            //string[] land = new string[]
+            //{
+            //    "Serbien", "Grekland", "USA", "Nederländerna"
+            //};
+
+            //Console.WriteLine("Inan sort: ");
+            //foreach (var l in land)
+            //{
+            //    Console.WriteLine(l);
+            //}
+            //Console.WriteLine();
+
+            //Array.Sort(land);
+
+            //Console.WriteLine("Efter sort: ");
+            //foreach(var l in land)
+            //{
+            //    Console.WriteLine(l);
+            //}
+
+            /*2.Sort och Reverse */
+            //int[] tal = { 6, 82, 35, 12 };
+            //Console.WriteLine("Inan sort: ");
+            //foreach (int i in tal)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine();
+            ////Array.Sort(tal);
+            ////Console.WriteLine("Efter sort: ");
+            ////foreach (int i in tal)
+            ////{
+            ////    Console.WriteLine(i);
+            ////}
+            ////Console.WriteLine();
+
+            //Array.Reverse(tal);
+            //Console.WriteLine("Efter reverse: ");
+            //foreach (int i in tal)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            /*3.Recursion */
+
+            //Countdown(50);
+
+            //void Countdown(int number)
+            //{
+            //    if (number == 0) return;
+
+            //    Console.WriteLine(number);
+            //    Countdown(number - 1);
+            //}
+
+            /*Uppgift 1*/
+
+            //Person("John");
+            //Person("Ema", "Anna", "Henrik");
+
+            //void Person(params string[] namn)
+            //{
+            //    foreach (string i in namn)
+            //        Console.WriteLine($"Hello, {i}!");
+            //}
+
+            /* Hantering av betyg - Rep2 */
+
+            string userInput;
+
+            do 
+            {
+                Console.WriteLine("Vill du beräkna betyg för elev? (ja/nej): ");
+                userInput = Console.ReadLine();
+
+                if (userInput == "ja")
+                {
+                    Console.WriteLine("Ange elevens poäng: ");
+                    int points = int.Parse(Console.ReadLine());
+                    char grade;
+
+                    if (points >= 90 && points <= 100)
+                        grade = 'A';
+                    else if (points >= 80 && points < 90)
+                        grade = 'B';
+                    else if (points >= 70 && points < 80)
+                        grade = 'C';
+                    else if (points >= 60 && points < 70)
+                        grade = 'D';
+                    else if (points >= 50 && points < 60)
+                        grade = 'E';
+                    else
+                        grade = 'F';
+
+                    Console.WriteLine($"Elevens poäng: {points} och betyg: {grade}.");     
+                }
+                else if (userInput != "nej")
+                    Console.WriteLine("Ogiltigt svar. Försök igen.");
+            } 
+            while (userInput != "nej");
+
+
+        }
+    }
+}
