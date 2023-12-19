4de4f9742690793db7ffc4363c0b9a9e7745f3a7
Nevena <nevena.kicanovic@gmail.com>
2023-12-19 14:58:21 +0100

    23_12_19

---
+++ Uppgift1_19dec/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Xml;
+
+namespace Uppgift1_19dec
+{
+    class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+    class UserManager
+    {
+        private List<User> users;
+
+        public UserManager()
+        {
+            users = new List<User>();
+        }
+
+        public void AddUser(User user)
+        {
+            users.Add(user);
+        }
+        public void InsertUsers ( int index, List<User> newUsers ) 
+        {
+            users.InsertRange( index, newUsers );
+        }
+        //RemoveUser()
+        //GetUserCount()
+        //ClearUser()
+        //DisplayUsers()
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserManager userManager = new UserManager();
+
+            //Lägg till användare
+            User user1 = new User("Alice", 30);
+            User user2 = new User("Bob", 25);
+            //User user3 = new User(namn, ålder);
+
+            userManager.AddUser(user1);
+            userManager.AddUser(user2);
+
+            //Lägg till flera användare på en angiven position
+            List<User> newUsers = new List<User>
+            {
+                new User ("Charlie", 35),
+                new User ("David", 40)
+            };
+
+            userManager.InsertUsers(1, newUsers);
+
+        }
+    }
+}
