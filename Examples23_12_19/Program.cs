1d301e5bd7827d3cea7afcd2dc8743ddc8c7ab61
Nevena <nevena.kicanovic@gmail.com>
2023-12-19 10:49:31 +0100

    23_12_19

---
+++ Examples23_12_19/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Examples23_12_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skapa - instansiera ett objekt av klass List<T>
+            List<int> my_list = new List<int>();
+
+            //Lägg till element - Add() metod
+            my_list.Add(1);
+            my_list.Add(10);
+            my_list.Add(100);
+            my_list.Add(1000);
+            my_list.Add(10000);
+            my_list.Add(100000);
+            my_list.Add(1000000);
+
+            foreach (int e in my_list) 
+            {
+                Console.WriteLine(e);
+            }
+
+            //Ta bort element av lista
+
+            //Count
+            Console.WriteLine($"Antal element: {my_list.Count}");
+            foreach (int e in my_list)
+            {
+                Console.WriteLine(e);
+            }
+
+            my_list.Remove(10);
+
+            Console.WriteLine($"Antal element efter remove: {my_list.Count}");
+            foreach (int e in my_list)
+            {
+                Console.WriteLine(e);
+            }
+            my_list.RemoveAt(4);
+            Console.WriteLine($"Antal element efter remove at: {my_list.Count}");
+            foreach (int e in my_list)
+            {
+                Console.WriteLine(e);
+            }
+
+            my_list.RemoveRange(0, 2);
+            Console.WriteLine($"Antal element efter remove range: {my_list.Count}");
+            foreach (int e in my_list)
+            {
+                Console.WriteLine(e);
+            }
+
+            my_list.Clear();
+            Console.WriteLine($"Antal element efter clear: {my_list.Count}");
+            foreach (int e in my_list)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine();
+
+            List<int> my_list2 = new List<int>();
+
+            my_list2.Add(496);
+            my_list2.Add(1000);
+            my_list2.Add(100);
+            my_list2.Add(10);
+            my_list2.Add(10000);
+            my_list2.Add(100000000);
+            my_list2.Add(10000000);
+            my_list2.Add(1000000);
+            my_list2.Add(00000);
+
+            //Utan sorterting
+            Console.WriteLine("Unsorted list: ");
+            foreach (int e in my_list2)
+            {
+                Console.WriteLine(e);
+            }
+
+            my_list2.Sort();
+
+            Console.WriteLine("Sorted list: ");
+            foreach (int e in my_list2)
+            {
+                Console.WriteLine(e);
+            }
+
+           
+        }
+    }
+}
---
+++ ExamplesDictionary/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace ExamplesDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cities = new Dictionary<string, string>()
+            {
+                {"UK","London, Manchester, Birmingham" },
+                {"USA", "Chicago, New York, Washington" },
+                {"India","Mumbai, New Delhi, Pune" }
+            };
+
+            foreach (var e in cities)
+                Console.WriteLine($"Key: {e.Key}, Value: {e.Value}");
+
+            Console.WriteLine();
+            Console.WriteLine(cities["UK"]); // värde för nyckel UK
+            //Console.WriteLine(cities["usa"]);
+
+            //kontrollera om nyckeln finns
+
+            if (cities.ContainsKey("France"))
+                Console.WriteLine(cities["France"]);
+            else
+                Console.WriteLine("Ingen element med nyckel \"France\" ");
+
+            Console.WriteLine("_____________________________________");
+            string result;
+
+            if (cities.TryGetValue("USA",out result))
+                Console.WriteLine(result);
+
+            Console.WriteLine("_____________________________________");
+            //använda ElementAt() att hämta key-value par med hjälp av index
+            for (int i = 0; i < cities.Count; i++)
+            {
+                Console.WriteLine($"Key: {cities.ElementAt(i).Key}, " +
+                    $"Value: {cities.ElementAt(i).Value}");
+            }
+
+            //Uppdatera element
+            cities["UK"] = "Liverpool, Bristol";
+            cities["USA"] = "Los Angeles, Boston";
+
+
+            if (cities.ContainsKey("France"))
+            {
+                cities["France"] = "Paris";
+            }
+
+            //Ta bort elements
+            cities.Remove("UK");
+
+            cities.Remove("France");
+
+            cities.Clear(); //tar bort dictionary
+
+        }
+    }
+}
