aa478d7a74ad7995feddca6a99a2881beafe7932
Nevena <nevena.kicanovic@gmail.com>
2023-12-29 11:27:48 +0100

    23_12_29

---
+++ CardinalToOrdinal/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CardinalToOrdinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunCardinalToOrdinal();
+
+            static string CardinalToOrdinal(int number)
+            {
+                int lastTwoDigits = number % 100;
+                switch (lastTwoDigits)
+                {
+                    case 11:
+                    case 12:
+                    case 13:
+                        return $"{number}th";
+                    default:
+                        int lastDigit = number % 10;
+                        string suffix = lastDigit switch
+                        {
+                            1 => "st",
+                            2 => "nd",
+                            3 => "rd",
+                            _ => "th"
+
+                        };
+                        return $"{number}{suffix}";
+                }
+            }
+
+            static void RunCardinalToOrdinal()
+            {
+                for (int number = 1; number <= 1100; number++)
+                {
+                    Console.WriteLine($"{CardinalToOrdinal(number)}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
