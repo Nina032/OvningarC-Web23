ea4aec3c2e23a2d52a5fd83f72e880da11abf029
Nevena <nevena.kicanovic@gmail.com>
2023-12-29 09:52:19 +0100

    23_12_29

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -95,7 +95,113 @@ namespace TicTacToe
             mPlayer1Turn ^= true;
 
             //Kontrollera vinnaren
+            CheckForWinner();
+        }
 
+        private void CheckForWinner()
+        {
+            #region Horizontal wins
+            //Kontrolera rader för vinnare
+
+            //Row 0
+            if (mResult[0] != MarkType.Free && (mResult[0] & mResult[1] & mResult[2]) == mResult[0])
+            {
+                //Spelet avslutas
+                mGameEnded = true;
+
+                //Ändra färg för de buttons
+                Button_0_0.Background = Button_1_0.Background = Button_2_0.Background = Brushes.Green;
+            }
+
+            //Row 1
+            if (mResult[3] != MarkType.Free && (mResult[3] & mResult[4] & mResult[5]) == mResult[3])
+            {
+                //Spelet avslutas
+                mGameEnded = true;
+
+                //Ändra färg för de buttons
+                Button_0_1.Background = Button_1_1.Background = Button_2_1.Background = Brushes.Green;
+            }
+
+            //Row 2
+            if (mResult[6] != MarkType.Free && (mResult[6] & mResult[7] & mResult[8]) == mResult[6])
+            {
+                //Spelet avslutas
+                mGameEnded = true;
+
+                //Ändra färg för de buttons
+                Button_0_2.Background = Button_1_2.Background = Button_2_2.Background = Brushes.Green;
+            }
+            #endregion
+
+            #region Vertical wins
+            //Column 0
+            if (mResult[0] != MarkType.Free && (mResult[0] & mResult[3] & mResult[6]) == mResult[0])
+            {
+                //Spelet avslutas
+                mGameEnded = true;
+
+                //Ändra färg för de buttons
+                Button_0_0.Background = Button_0_1.Background = Button_0_2.Background = Brushes.Green;
+            }
+
+            //Column 1
+            if (mResult[1] != MarkType.Free && (mResult[1] & mResult[4] & mResult[7]) == mResult[1])
+            {
+                //Spelet avslutas
+                mGameEnded = true;
+
+                //Ändra färg för de buttons
+                Button_1_0.Background = Button_1_1.Background = Button_1_2.Background = Brushes.Green;
+            }
+
+            //Column 2
+            if (mResult[2] != MarkType.Free && (mResult[2] & mResult[5] & mResult[8]) == mResult[2])
+            {
+                //Spelet avslutas
+                mGameEnded = true;
+
+                //Ändra färg för de buttons
+                Button_2_0.Background = Button_2_1.Background = Button_2_2.Background = Brushes.Green;
+            }
+            #endregion
+
+            #region Diagonal wins
+            //Top left to bottom right
+            if (mResult[0] != MarkType.Free && (mResult[0] & mResult[4] & mResult[8]) == mResult[0])
+            {
+                //Spelet avslutas
+                mGameEnded = true;
+
+                //Ändra färg för de buttons
+                Button_0_0.Background = Button_1_1.Background = Button_2_2.Background = Brushes.Green;
+            }
+
+            //Top right to bottom left
+            if (mResult[2] != MarkType.Free && (mResult[2] & mResult[4] & mResult[6]) == mResult[2])
+            {
+                //Spelet avslutas
+                mGameEnded = true;
+
+                //Ändra färg för de buttons
+                Button_2_0.Background = Button_1_1.Background = Button_0_2.Background = Brushes.Green;
+            }
+            #endregion
+
+            #region No winners
+            //Kontrollera att vi har ingen vinnare och att "board" är ifylld
+            if (!mResult.Any(f => f == MarkType.Free))
+            {
+                //Avsluta spelet
+                mGameEnded = true;
+
+                //Ändra färg till orange
+                Container.Children.Cast<Button>().ToList().ForEach(button =>
+                {
+                    button.Background = Brushes.Orange;
+                });
+            }
+            #endregion
         }
     }
 }
